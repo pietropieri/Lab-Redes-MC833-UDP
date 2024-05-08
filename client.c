711546e59009195d14b183d7c5dde7d60f0d379f
Francisco Guimaraes Pena <ra234589@neruda.lab.ic.unicamp.br>
2024-05-08 14:26:46 -0300

    add tcp

--- client.c
+++ client.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #define SIZE 1024
+#define TCP_PORT 9090
+#define UDP_PORT 8080
 
 void write_file(int sockfd, struct sockaddr_in addr) {
     int n;
@@ -28,13 +31,45 @@ void write_file(int sockfd, struct sockaddr_in addr) {
     fclose(fp);
 }
 
-void send_command(int sockfd, struct sockaddr_in addr, const char* command) {
-    sendto(sockfd, command, strlen(command) + 1, 0, (struct sockaddr*)&addr, sizeof(addr)); // +1 to include the null terminator
+void send_command_udp(int sockfd, struct sockaddr_in addr, const char* command) {
+    sendto(sockfd, command, strlen(command) + 1, 0, (struct sockaddr*)&addr, sizeof(addr));
+}
+
+void request_tcp(int index) {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    char index_str[10];
+    sprintf(index_str, "%d", index);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(TCP_PORT);
+    
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        close(sock);
+        return;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        close(sock);
+        return;
+    }
+
+    send(sock, index_str, strlen(index_str), 0);
+    valread = read(sock, buffer, 1024);
+    printf("Received: %s\n", buffer);
+    
+    close(sock);
 }
 
 int main(void) {
-    char *ip = "127.0.0.1";
-    const int port = 8080;
     int server_sockfd;
     struct sockaddr_in server_addr;
 
@@ -44,24 +79,31 @@ int main(void) {
         exit(1);
     }
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(UDP_PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     int continue_running = 1;
     char input[10];
 
     while (continue_running) {
-        printf("Enter command (1: Download MP3, 2: Close server, 0: Exit client): ");
+        printf("Enter command (1: Download MP3, 2: Close server, 3: Get list item, 0: Exit client): ");
         fgets(input, 10, stdin);
         input[strcspn(input, "\n")] = 0;  // Remove newline character
 
         if (strcmp(input, "1") == 0) {
             printf("[REQUEST] Requesting MP3 file from server.\n");
-            send_command(server_sockfd, server_addr, "1");
+            send_command_udp(server_sockfd, server_addr, "1");
             write_file(server_sockfd, server_addr);
         } else if (strcmp(input, "2") == 0) {
             printf("[REQUEST] Requesting to close the server.\n");
-            send_command(server_sockfd, server_addr, "2");
+            send_command_udp(server_sockfd, server_addr, "2");
+            continue_running = 0;  // Optionally exit the client after server shutdown
+        } else if (strcmp(input, "3") == 0) {
+            printf("Enter index (0-7): ");
+            int index;
+            scanf("%d", &index);  // Read the index from user
+            getchar(); // consume newline character
+            request_tcp(index);
         } else if (strcmp(input, "0") == 0) {
             printf("[CLOSING] Exiting client.\n");
             continue_running = 0;
--- server.c
+++ server.c
@@ -3,78 +3,136 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define SIZE 1024
+#define TCP_PORT 9090
+#define UDP_PORT 8080
+
+char* list[8] = {"aaaa", "bbbb", "cccc", "dddd", "eeee", "ffff", "gggg", "hhhh"};
 
 void send_file_data(FILE* fp, int sockfd, struct sockaddr_in addr) {
-  int n;
-  char buffer[SIZE];
-  socklen_t addr_size = sizeof(addr);
+    int n;
+    char buffer[SIZE];
+    socklen_t addr_size = sizeof(addr);
 
-  while (fread(buffer, 1, SIZE, fp) > 0) {
-    printf("[SENDING] Data of %d bytes\n", strlen(buffer));
-    n = sendto(sockfd, buffer, SIZE, 0, (struct sockaddr*)&addr, addr_size);
-    if (n == -1) {
-      perror("[ERROR] Sending data to the client.");
-      exit(1);
+    while (fread(buffer, 1, SIZE, fp) > 0) {
+        sendto(sockfd, buffer, SIZE, 0, (struct sockaddr*)&addr, addr_size);
     }
-  }
 
-  // Sending the 'END'
-  strcpy(buffer, "END");
-  sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&addr, addr_size);
+    // Sending the 'END'
+    strcpy(buffer, "END");
+    sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&addr, addr_size);
 
-  fclose(fp);
+    fclose(fp);
+}
+
+void* handle_udp(void* arg) {
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    char buffer[SIZE];
+    int recv_len;
+    socklen_t addr_size;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(UDP_PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("UDP server started...\n");
+
+    while (1) {
+        addr_size = sizeof(client_addr);
+        recv_len = recvfrom(sockfd, buffer, SIZE, 0, (struct sockaddr*)&client_addr, &addr_size);
+        buffer[recv_len] = '\0';
+
+        if (strcmp(buffer, "1") == 0) {
+            FILE* fp = fopen("server.mp3", "rb");
+            if (fp == NULL) {
+                perror("File open failed");
+            } else {
+                send_file_data(fp, sockfd, client_addr);
+            }
+        } else if (strcmp(buffer, "2") == 0) {
+            printf("Shutdown command received. Exiting.\n");
+            break;
+        }
+    }
+
+    close(sockfd);
+    return NULL;
+}
+
+void* handle_tcp(void* arg) {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    char buffer[SIZE] = {0};
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(TCP_PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        int valread = read(new_socket, buffer, SIZE);
+        int index = atoi(buffer);
+        if (index >= 0 && index < 8) {
+            send(new_socket, list[index], strlen(list[index]), 0);
+        } else {
+            send(new_socket, "Invalid index", strlen("Invalid index"), 0);
+        }
+        close(new_socket);
+    }
+
+    close(server_fd);
+    return NULL;
 }
 
 int main() {
-  char* ip = "127.0.0.1";
-  const int port = 8080;
-  int server_sockfd;
-  struct sockaddr_in server_addr, client_addr;
-  char buffer[SIZE];
-  int e;
+    pthread_t thread_id_tcp, thread_id_udp;
 
-  server_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (server_sockfd < 0) {
-    perror("[ERROR] Socket error");
-    exit(1);
-  }
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(port);
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-
-  e = bind(server_sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if (e < 0) {
-    perror("[ERROR] Bind error");
-    exit(1);
-  }
-
-  printf("[STARTING] UDP File Server started. Waiting for client commands...\n");
-
-  int continue_running = 1;
-  while (continue_running) {
-    socklen_t addr_size = sizeof(client_addr);
-    recvfrom(server_sockfd, buffer, SIZE, 0, (struct sockaddr*)&client_addr, &addr_size);
-
-    if (strcmp(buffer, "1") == 0) {
-      printf("[INFO] Client requested the MP3 file.\n");
-      FILE* fp = fopen("server.mp3", "rb");
-      if (fp == NULL) {
-        perror("[ERROR] File not found");
-        exit(1);
-      }
-      send_file_data(fp, server_sockfd, client_addr);
-    } else if (strcmp(buffer, "2") == 0) {
-      printf("[INFO] Client requested to close the server.\n");
-      continue_running = 0;
-    } else {
-      printf("[ERROR] Unknown command received: %s\n", buffer);
+    if (pthread_create(&thread_id_tcp, NULL, handle_tcp, NULL) != 0) {
+        printf("Failed to create TCP thread\n");
+        return 1;
     }
-  }
+    if (pthread_create(&thread_id_udp, NULL, handle_udp, NULL) != 0) {
+        printf("Failed to create UDP thread\n");
+        return 1;
+    }
 
-  printf("[CLOSING] Closing the server.\n");
-  close(server_sockfd);
+    pthread_join(thread_id_tcp, NULL);
+    pthread_join(thread_id_udp, NULL);
 
-  return 0;
+    return 0;
 }
